b9d803528596f7d92de2106de590a53474f37d00
shalev elia <u9011011@bsmch.net>
2021-06-16 21:35:11 +0300

    export and import sniffing data

--- Slave.py
+++ Slave.py
@@ -1,49 +1,50 @@
 import socket
-from scapy.all import *
-from scapy.layers.inet import IP,Ether
+from scapy.all import sniff
+from scapy.layers.inet import IP, Ether
 import uuid
 import json
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 MESSAGE = b"Hello, World!"
-Packets=[]
-SLAVEMAC=(':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) for ele in range(0,8*6,8)][::-1]))
-PacketsToMaster=[]
-class packt:
+Packets = []
+SLAVEMAC = (':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) for ele in range(0, 8 * 6, 8)][::-1]))
+PacketsToMaster = []
+
+
+class packet:
     def __init__(self, IP, PORT):
-        self.IP=IP
-        self.PORT=PORT
+        self.IP = IP
+        self.PORT = PORT
 
     def __str__(self):
-        return  "port:%s ip:%s"% (self.PORT, self.IP)
-
+        return "port:%s ip:%s" % (self.PORT, self.IP)
 
 
 def main():
     print(SLAVEMAC)
     while True:
-        capture = sniff(prn=pkt_callback )
+        capture = sniff(prn=pkt_callback)
+
 
 def pktToJson(pkt):
-    return {"ip":pkt.IP,"port":pkt.PORT}
+    return {"ip": pkt.IP, "port": pkt.PORT}
+
+
 def process_pkt(pkt):
-    ip=""
-    port=""
     try:
-        if(pkt[0].src==SLAVEMAC):
-            port=pkt[IP].dport
-            ip=pkt[IP].dst
+        if (pkt[0].src == SLAVEMAC):
+            port = pkt[IP].dport
+            ip = pkt[IP].dst
 
         if (pkt[0].dst == SLAVEMAC):
-            port=pkt[IP].sport
+            port = pkt[IP].sport
             ip = pkt[IP].src
+    except IndexError:
+        return None
 
+    return packet(ip, port)
 
-    except:
-        pass
-
-    return packt(ip,port)
 
 def pkt_callback(pkt):
     global Packets
@@ -55,18 +56,20 @@ def pkt_callback(pkt):
     else:
         return
 
-
-    if(len(Packets)==50): #50 packets per round
-        ports=[]
+    if len(Packets) == 50:  # 50 packets per round
+        ports = []
         for pck in Packets:
-            PacketsToMaster.append(process_pkt(pck))
-        test=json.dumps(PacketsToMaster,default=pktToJson)
+            procPKT = process_pkt(pck)
+            if procPKT is not None:
+                PacketsToMaster.append(procPKT)
+        test = json.dumps(PacketsToMaster, default=pktToJson)
         sock = socket.socket(socket.AF_INET,
                              socket.SOCK_DGRAM)
 
-        sock.sendto(test.encode(), (UDP_IP,5005))
-        print("sent")
-        PacketsToMaster=[]
-        Packets=[]
+        sock.sendto(test.encode(), (UDP_IP, 5005))
+        PacketsToMaster = []
+        Packets = []
+
+
 if __name__ == '__main__':
     main()
--- master.py
+++ master.py
@@ -5,29 +5,48 @@ UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 
 dat = {}
-#172.16.11.8:[ip:(10.0.0.10:20,10.0.0.4:43),port:(80:35,443:245)]
-def process_packets(packets,Slaveip):
+i = 3
+
+def load_data():
+    global dat
+    with open('data.json', 'r') as f:
+        dat = json.load(f)
+
+
+def export_data():
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(dat, f, ensure_ascii=False, indent=4)
+
+
+def process_packets(packets, Slaveip):
     global dat
     for ptk in packets:
         if Slaveip in dat:
-            if( ptk['ip'] in dat[Slaveip]["ips"]):
-                dat[Slaveip]["ips"][ptk['ip']]+=1
+            if ptk['ip'] in dat[Slaveip]["ips"]:
+                dat[Slaveip]["ips"][ptk['ip']] += 1
             else:
-                dat[Slaveip]["ips"][ptk['ip']]=1
-            if( ptk['port'] in dat[Slaveip]["ports"]):
-                dat[Slaveip]["ports"][ptk['port']]+=1
+                dat[Slaveip]["ips"][ptk['ip']] = 1
+            if ptk['port'] in dat[Slaveip]["ports"]:
+                dat[Slaveip]["ports"][ptk['port']] += 1
             else:
-                dat[Slaveip]["ports"][ptk['port']]=1
+                dat[Slaveip]["ports"][ptk['port']] = 1
         else:
-            dat[Slaveip]={"ips":{ptk["ip"]:1},"ports":{ptk["port"]:1}}
+            dat[Slaveip] = {"ips": {ptk["ip"]: 1}, "ports": {ptk["port"]: 1}}
+    export_data()
+
 
 def main():
+    global dat
+    load_data()
     sock = socket.socket(socket.AF_INET,  # Internet
                          socket.SOCK_DGRAM)  # UDP
     sock.bind((UDP_IP, UDP_PORT))
+
     while True:
-        data, addr = sock.recvfrom(128000)  # buffer size is 1024 bytes
+        data, adder = sock.recvfrom(128000)  # buffer size is 1024 bytes
         packets = json.loads(data)
-        process_packets(packets,addr[0])
+        process_packets(packets, adder[0])
+
+
 if __name__ == '__main__':
     main()
